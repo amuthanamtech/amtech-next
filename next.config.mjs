552cc61fb76d9f73e8064a76146b54fc5e0e06fc
prakashvengad <prakashvengad7575@gmail.com>
2025-07-24 16:54:11 +0530

    Force Node.js 20 and fix dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -9,16 +9,14 @@ if (process.env.NODE_ENV === 'development') {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   experimental: {
-    appDir: true,
-    SMTP_HOST: process.env.SMTP_HOST,
-    SMTP_PORT: process.env.SMTP_PORT,
+     SMTP_HOST: process.env.SMTP_HOST,
+    SMTP_PORT:process.env.SMTP_PORT,
     SMTP_SECURE: process.env.SMTP_SECURE,
     SMTP_USER: process.env.SMTP_USER,
     SMTP_PASSWORD: process.env.SMTP_PASSWORD,
     SMTP_FROM_EMAIL: process.env.SMTP_FROM_EMAIL,
-    SMTP_TO_EMAIL: process.env.SMTP_TO_EMAIL,
-  }
+    SMTP_TO_EMAIL :process.env.SMTP_TO_EMAIL,
+   
 };
 
 export default nextConfig;
