180da13b8239a44bf81963fdec0bfbb08aea240f
Amuthan Sakthivel <amuthan.s@amtechdigital.co>
2025-07-25 00:36:10 +0530

    build fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-
-// Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
-// (when running the application with `next dev`), for more information see:
-// https://github.com/cloudflare/next-on-pages/blob/main/internal-packages/next-dev/README.md
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   experimental: {
-    appDir: true,
+  env: {
     SMTP_HOST: process.env.SMTP_HOST,
     SMTP_PORT: process.env.SMTP_PORT,
     SMTP_SECURE: process.env.SMTP_SECURE,
