a9ab8173f999432289554b9a328aa573f815cc7e
prakashvengad <prakashvengad7575@gmail.com>
2025-07-25 17:19:03 +0530

    footerchange

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
