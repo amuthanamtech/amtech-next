78962372f2247a74a31f5314df5a0979b2f6d86b
prakashvengad <prakashvengad7575@gmail.com>
2025-08-08 19:39:25 +0530

    fix the error

--- src/app/api/knowledge/route.ts
+++ src/app/api/knowledge/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import path from "path";
 import fs from "fs";
 
-export const runtime = 'edge';
+
 export async function GET(request: NextRequest) {
   try {
     const filePath = path.join(process.cwd(), "data", "knowledge.md");
